2b17cf3838294c9e0539decaa4209852c4629549
LiuAoXiang233 <liuaoxiang233@163.com>
2024-09-22 08:25:07 +0800

    9/22 Inventory System ...

---
+++ Source/GAS_Learning_Demo/Public/Inventory/UInventory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "UInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_LEARNING_DEMO_API UUInventory : public UObject
+{
+	GENERATED_BODY()
+	
+};
